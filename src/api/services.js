ef0734a31cc03ba1cf1705520c12029aa8adf6bb
thien <thienng93@gmail.com>
2020-07-22 21:55:31 -0700

    change the aws lambda url

--- src/api/services.js
+++ src/api/services.js
@@ -4,12 +4,12 @@ export const calculatePaycheck = salaryWorkSavingInfo => {
     var config = {
         headers: {'Access-Control-Allow-Origin': '*'}
     };
-    // return axios.post('https://qenzxs2x29.execute-api.us-west-2.amazonaws.com/Prod/calculate_paycheck', {salaryWorkSavingInfo})
-    //     .then((resp) => {
-    //         return resp.data
-    //     })
-    return axios.post('http://localhost:5000/calculate_paycheck', {salaryWorkSavingInfo})
-    .then((resp) => {
-        return resp.data
-    })
+    return axios.post('https://qenzxs2x29.execute-api.us-west-2.amazonaws.com/Prod/calculate_paycheck', {salaryWorkSavingInfo})
+        .then((resp) => {
+            return resp.data
+        })
+    // return axios.post('http://localhost:5000/calculate_paycheck', {salaryWorkSavingInfo})
+    // .then((resp) => {
+    //     return resp.data
+    // })
 }
